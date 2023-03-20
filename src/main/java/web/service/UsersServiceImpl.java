97dfe91d03a772b7340e53aebd9b05ef4be4ed94
DrKringe <Tuork@yandex.ru>
2023-03-20 19:02:50 +0300

    поправил

--- src/main/java/web/service/UsersServiceImpl.java
+++ src/main/java/web/service/UsersServiceImpl.java
@@ -8,36 +8,40 @@ import web.model.Users;
 
 import java.util.List;
 @Service
-@Transactional
 public class UsersServiceImpl implements UsersService{
     private final DAO userDAO;
 
-    @Autowired
+
     public UsersServiceImpl(DAO userDAO) {
         this.userDAO = userDAO;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Users> getAllUsers() {
         return userDAO.getAllUsers();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Users getUserId(long id) {
         return userDAO.getUserId(id);
     }
 
     @Override
+    @Transactional
     public void addUser(Users user) {
         userDAO.addUser(user);
     }
 
     @Override
+    @Transactional
     public void updateUser(Users user) {
         userDAO.updateUser(user);
     }
 
     @Override
+    @Transactional
     public Users removeUser(long id) {
         return userDAO.removeUser(id); }
 }
